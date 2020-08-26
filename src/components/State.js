d199146a3bc4f665073f965baf332131c03893a7
akashgupta1116 <yoakash6@gmail.com>
2020-08-26 23:01:37 +0530

    final push

--- src/components/State.js
+++ src/components/State.js
@@ -1,81 +1,8 @@
-// import React, { Component } from 'react';
-// import { Redirect } from 'react-router';
-
-
-// class State extends Component {
-//     constructor(props){
-//         super(props)
-//         this.state={
-//             stateData:[],
-//             redirect:false,
-//             searchedState:this.props.state,
-//             apiData:[]
-//         }
-//     }
-//     static getDerivedStateFromProps=(nextProps, prevProps)=>{
-//         return{searchedState: nextProps.state};
-//     }
-//     componentDidMount(){
-//         console.log('searcedState',this.state.searchedState)
-//         fetch('https://api.covid19india.org/data.json')
-//         .then(resp=>resp.json())
-//         .then(result=>{
-//         //    const stateData = result.statewise.filter(item=>{
-//         //         return  this.state.searchedState.charAt(0).toUpperCase() + this.state.searchedState.slice(1) === item.state
-//         //     })
-//         //     console.log('state',stateData);
-//         //     if(stateData.length){
-                
-//         //         this.setState({
-//         //             stateData:stateData
-//         //         })
-//         //     }
-//         //     else{
-//         //         console.log('sorry');
-//         //         this.setState({
-//         //             redirect:true
-//         //         })
-//         //     }
-//             this.setState({
-//                 apiData:result.statewise
-//             })
-//         })
-//     }
-//     filterData = () =>{
-//         const stateData = this.state.apiData.filter(item=>{
-//             return this.state.searchedState.charAt(0).toUpperCase() + this.state.searchedState.slice(1) === item.state
-//         })
-//         if(stateData.length){
-//              this.setState({
-//                  stateData:stateData
-//              })
-//         }
-//     }
-//     // componentDidUpdate(){
-//     //     this.filterData();
-//     // }
-    
-//     render() {
-//         // this.filterData();
-//         console.log('render-searchedState',this.state.searchedState)
-//         if(this.state.redirect){
-//             return <Redirect to='/'/>
-//         }
-//         return (
-//             <div>
-//                 {this.state.stateData.length?this.state.stateData[0].state:''}
-//             </div>
-//         );
-//     }
-// }
-
-// export default State;
-
 import React,{useState,useEffect} from 'react';
 import { Container, Row,Col } from 'reactstrap';
 import AppCard from './AppCard'
 import BarChart from './BarChart'
-import Barchart from './BarChart';
+
 
 
 const State = props => {
